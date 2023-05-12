71143124badbb10d7cd768c45683330a3534c6fe
Samuel Debeb <samuel.debebe.27@gmail.com>
2023-05-12 11:22:44 +0300

    Mony transfer API project saved on Local repository

---
+++ src/main/java/com/act/monytransferapi/MonyTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.act.monytransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonyTransferApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonyTransferApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/act/monytransferapi/MonyTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.act.monytransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonyTransferApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
